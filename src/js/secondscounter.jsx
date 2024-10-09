2b3326f30ec087468a8046e5fac3e311de8e4ea7
Katerine Céspedes <katerinecesbal@hotmail.com>
2024-10-09 19:06:59 +0000

    reloj

--- src/js/secondscounter.jsx
+++ src/js/secondscounter.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+
 
 const Card = (props) =>{
     return(
-        <div class="card text-bg-dark mb-3">            
-            <div class="card-body">
-                <h5 class="card-title">{props.digit}</h5>
+    
+        <div class="card text-bg-dark my-3 mx-3 " style={{width:"100px"}} >            
+            <div class="card-body d-flex justify-content-center align-items center" style={{backgroundColor:"black"}}>
+                <h5 class="card-title text-center" style={{fontSize:"90px"}}>{props.digit}</h5>
             </div>
         </div>
     )
 }
 
+
 const SecondsCounter = (props) => {
     return (
-    <div className = "d-flex">
-
+    <div className = "d-flex justify-content-center" style={{backgroundColor:"black"}}>
+            <Card digit={<FontAwesomeIcon icon={faClock} style={{color: "#ffffff",}} />} />
             <Card digit={Math.floor(props.counter/10000 %10)}/>
            <Card digit={Math.floor(props.counter/10000 %10)}/>
            <Card digit={Math.floor(props.counter/1000 %10)}/>
